6311b7930752deeeea4910a644f01db18acd656e
salina225 <skhanom@madisoncollege.edu>
2020-10-01 11:07:37 -0500

    generated-pizza-toppings
    ng generate component

---
+++ src/app/pizza-topping/pizza-topping.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PizzaToppingComponent } from './pizza-topping.component';
+
+describe('PizzaToppingComponent', () => {
+  let component: PizzaToppingComponent;
+  let fixture: ComponentFixture<PizzaToppingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PizzaToppingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PizzaToppingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
