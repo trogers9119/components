99e09e2a2089a55967d31e7842bb9863d394c611
salina225 <skhanom@madisoncollege.edu>
2020-10-01 11:50:29 -0500

    A

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+export interface pizzatoppingdisplay {
+  name: string;
+  price: Number;
+  checked: Boolean;
+
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class PizzaService {
 
   constructor() { }
+
+  public loadpizzatopping(): pizzatoppingdisplay[] {
+    const pizzatoppingfromwebservice = [
+      {name: 'peperoni', price: 1.5};
+      {name: 'olives', price: 2}
+
+    ];
+    return pizzatoppingfromwebservice.map(x => ({
+      ...x
+      , checked: false;
+    }))
+  }
 }
