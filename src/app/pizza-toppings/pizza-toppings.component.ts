d28d4c8da775cabf26b2fbddfcbe634d83f164b6
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-10-01 11:35:30 -0500

    Using our pizza service for the first time...

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService, PizzaToppingDisplay } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+  // DI Magic ! ! !
+  constructor(public pizzaSvc: PizzaService) { }
 
   ngOnInit(): void {
+    const pizzaToppings = this.pizzaSvc.loadPizzaToppings();
+    console.log(pizzaToppings);
   }
-
 }
