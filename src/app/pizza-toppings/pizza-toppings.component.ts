c1f16a905a4a0ad73a36fa722da5d1d9090e84fb
MikeIsNewToTheCity <mikevilleneuve@gmail.com>
2020-10-01 12:01:14 -0500

    Calculate total working...
    got calculate total working

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -16,4 +16,17 @@ export class PizzaToppingsComponent implements OnInit {
   }
 
   pizzaToppings: PizzaToppingDisplay[] = [];
+
+  total = 0;
+
+  public calculateTotal() {
+    // console.log('here');    
+
+    this.total = this.pizzaToppings
+      .filter(x => x.checked)
+      .reduce(
+        (acc, x) => acc + x.price
+        , 0
+      );
+  }
 }
