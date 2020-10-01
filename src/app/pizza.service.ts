56bca490a3efba6540bc58050cc7a7af44d87ab8
mmiatech <mmiatech@madisoncollege.edu>
2020-10-01 11:35:40 -0500

    using the pizza-service for the first time

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
+export interface PizzaToppingDisplay {
+    name: string;
+    price: number;
+    checked: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaService {
 
   constructor() { }
+
+  public loadPizzaToppings(): PizzaToppingDisplay[] {
+
+      // fake data, should come from some loadPizzaToppings
+      const pizzaToppingsFromWebService = [
+          {name: 'Pepperoni', price: 1.5}
+          , {name: 'Olives', price: 2}
+      ];
+
+      return pizzaToppingsFromWebService.map(x => ({
+          ...x
+          , checked: false
+      }));
+  }
+
 }
