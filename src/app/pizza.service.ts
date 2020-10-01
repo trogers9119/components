ae366dabe92aefddb199614068eb060e2474f02c
MikeIsNewToTheCity <mikevilleneuve@gmail.com>
2020-10-01 11:35:38 -0500

    Using pizza service for first time

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
 
+export interface PizzaToppingDisplay {
+  name: string;
+  price: number;
+  checked: boolean; 
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaService {
 
   constructor() { }
+
+  public loadPizzaToppings(): PizzaToppingDisplay[] {
+
+    // fake data that would really come from pizza 
+    // site (i.e. Dominoes Pizza) API
+    const pizzaToppingsFromWebService = [
+      { name: 'Pepperoni', price: 1.5 }
+      , { name: 'Olives', price: 2 }
+    ];
+
+    return pizzaToppingsFromWebService.map(x => ({
+      ...x
+      , checked: false
+    }));
+  }
+  
 }
