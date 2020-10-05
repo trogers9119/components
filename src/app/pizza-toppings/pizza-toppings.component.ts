3803ecf94d2b3b931e0a0dd5748ca49f118cda43
mcstagg <32423173+mcstagg@users.noreply.github.com>
2020-10-05 18:08:57 -0500

    Calculate total working...
    Forgot Forms Module

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -17,4 +17,17 @@ export class PizzaToppingsComponent implements OnInit {
 
   pizzaToppings: PizzaToppingDisplay[] = [];
 
+  total = 0;
+
+  public calculateTotal() {
+    //console.log('here');
+
+    this.total = this.pizzaToppings
+        .filter(x => x.checked)
+        .reduce(
+            (acc, x) => acc + x.price
+            , 0
+        )
+  }
+
 }
