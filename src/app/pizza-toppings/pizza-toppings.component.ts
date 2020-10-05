0657a5af3fc69ae27de49d1f9e4cfc9e2d81ad6b
salina225 <skhanom@madisoncollege.edu>
2020-10-05 17:19:31 -0500

    forms module works!!

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {PizzaService, PizzaToppingDisplay} from '../pizza.service';
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public pizzaSvc: PizzaService) { }
 
   ngOnInit(): void {
-  }
+  this.pizzaToppings = this.pizzaSvc.loadPizzaToppings();
 
+  }
+pizzaToppings: PizzaToppingDisplay[] = []
+
+total = 0;
+
+public calculateTotal() {
+  //console.log('here');
+  this.total = this.pizzaToppings
+  .filter(x => x.checked)
+  .reduce(
+    (acc, x) => acc + x.price
+    , 0
+  );
+}
 }
