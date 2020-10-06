ae98af88c0280673be3533c3af3f41409d04b5ae
salina225 <skhanom@madisoncollege.edu>
2020-10-06 11:28:02 -0500

    wow..template can be used instead of url
    never done this way

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -4,6 +4,7 @@ import { PizzaService, PizzaToppingDisplay } from '../pizza.service';
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
+  //template: '<h1>No way</h1>',
   styleUrls: ['./pizza-toppings.component.css']
 })
 export class PizzaToppingsComponent implements OnInit {
