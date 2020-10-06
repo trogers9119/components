3041cd2bd5abbf7a74423075d9a2d58bb03a3dae
gkatuwal@madisoncollege.edu <Kurkuntar1>
2020-10-05 19:52:14 -0500

    generate pizza-toppings
    ng generate component pizza-toppings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
+import { PizzaTopingsComponent } from './pizza-topings/pizza-topings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    PizzaTopingsComponent
   ],
   imports: [
     BrowserModule
