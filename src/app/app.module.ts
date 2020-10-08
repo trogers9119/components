4903df00e56ee6c1f40ffc28e46f9e0ccbb35078
albertmensah <DMensah@madisoncollege.edu>
2020-10-08 15:59:25 -0500

    Generated component...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { TjsSubtractComponent } from './tjs-subtract/tjs-subtract.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    TjsSubtractComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tjs-subtract/tjs-subtract.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TjsSubtractComponent } from './tjs-subtract.component';
+
+describe('TjsSubtractComponent', () => {
+  let component: TjsSubtractComponent;
+  let fixture: ComponentFixture<TjsSubtractComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TjsSubtractComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TjsSubtractComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
