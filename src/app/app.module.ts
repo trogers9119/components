abcfdee7e46da188c20048064be67a08599c7e5f
mmiatech <mmiatech@madisoncollege.edu>
2020-10-08 10:53:34 -0500

    styled button and checkboxes w/ angular mat styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +20,8 @@ import {MatCardModule} from '@angular/material/card';
     , FormsModule
     , BrowserAnimationsModule
     , MatCardModule
+    , MatCheckboxModule
+    , MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
