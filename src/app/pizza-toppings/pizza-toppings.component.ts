f0f34c1e0ca093185b0735e55474d1142c5d676d
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2020-10-08 17:41:20 -0500

    WOW template can be used instead of templateUrl
    Talked about DI and overiding templateUrl

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -4,11 +4,16 @@ import { PizzaService, PizzaToppingDisplay } from '../pizza.service';
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
+  // template: '<h1>No Way</h1>',
   styleUrls: ['./pizza-toppings.component.css']
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  //DI Magic ! ! !
+  //DI Magic ! ! !     pizzaSvc is the dependency of type PizzaService
+
+  //For each new PizzaToppingsComponent you need to pass it a pizzaSvc,
+
+  //PizzaToppingsComponent depends on pizzaSvc
   constructor(public pizzaSvc: PizzaService) { }
 
   ngOnInit(): void {
