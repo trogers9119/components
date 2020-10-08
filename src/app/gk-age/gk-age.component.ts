0ef68ab8164434e425629c48d16a70e2bbdf8748
gkatuwal@madisoncollege.edu <Kurkuntar1>
2020-10-08 17:33:01 -0500

    added calculate age
    feuuuu

--- src/app/gk-age/gk-age.component.ts
+++ src/app/gk-age/gk-age.component.ts
@@ -4,12 +4,23 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-gk-age',
   templateUrl: './gk-age.component.html',
   styleUrls: ['./gk-age.component.css']
+
 })
 export class GkAgeComponent implements OnInit {
-
+  public selectedAge: Date;
+  public age: number;
   constructor() { }
 
   ngOnInit(): void {
   }
+  public CalculateAge(): void
+     {
+         if(this.birthdate){
+            var timeDiff = Math.abs(Date.now() - new Date(this.birthdate).getTime());
+            //Used Math.floor instead of Math.ceil
+            //so 26 years and 140 days would be considered as 26, not 27.
+            this.age = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+        }
+    }
 
 }
