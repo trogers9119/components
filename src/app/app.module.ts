d959f22929f83e93cafe8ece7b1ae41bc7cc0a1a
gkatuwal@madisoncollege.edu <Kurkuntar1>
2020-10-08 16:28:24 -0500

    create a age componnent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { GkAgeComponent } from './gk-age/gk-age.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    GkAgeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/gk-age/gk-age.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gk-age',
+  templateUrl: './gk-age.component.html',
+  styleUrls: ['./gk-age.component.css']
+})
+export class GkAgeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
