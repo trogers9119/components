30177d11c07fc6e882011163362c085b44b0af67
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2020-10-09 15:54:20 -0500

    Added new component
    Calculate a tip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { TipCalculatorComponent } from './tip-calculator/tip-calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    TipCalculatorComponent
   ],
   imports: [
     BrowserModule
