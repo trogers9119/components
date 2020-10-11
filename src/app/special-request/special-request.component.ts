79ce35a3c32f5a089434633288764d4f7877338d
salina225 <skhanom@madisoncollege.edu>
2020-10-11 13:08:15 -0500

    ng generate component special-request

---
+++ src/app/special-request/special-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-special-request',
+  templateUrl: './special-request.component.html',
+  styleUrls: ['./special-request.component.css']
+})
+export class SpecialRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
