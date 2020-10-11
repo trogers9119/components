f9c50f4a7b307d0516ba9dc8617cfa1787632b9a
mmiatech <mmiatech@madisoncollege.edu>
2020-10-11 13:58:40 -0500

    generated multiplication component
    ng generate component multiplication

---
+++ src/app/multiplication/multiplication.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multiplication',
+  templateUrl: './multiplication.component.html',
+  styleUrls: ['./multiplication.component.css']
+})
+export class MultiplicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
