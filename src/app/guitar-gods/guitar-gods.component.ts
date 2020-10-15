df76d1748fc29e4d2b76a97699f6ffaf6b16c302
mcstagg <32423173+mcstagg@users.noreply.github.com>
2020-10-14 22:40:28 -0500

    patternOne regex working
    patterns for switch statement starting to work

--- src/app/guitar-gods/guitar-gods.component.ts
+++ src/app/guitar-gods/guitar-gods.component.ts
@@ -7,59 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuitarGodsComponent implements OnInit {
 
-  firstName = '';
-  god;
+  firstName;
+  displayGod = 'white';
+
   createGod = () => {
-    if (this.firstName.substring(0,1) == 'a') {
-      this.god = this.stevieIsYourGod();
-    } else if (this.firstName.substring(0,1) == 'j') {
-      this.god = this.jimmyIsYourGod();
-    } else if (this.firstName.substring(0,1) == 'e') {
-      this.god = this.nancyIsYourGod();
-    } else {
-      this.god = this.jimiIsYourGod();
-    }
+    let theDecider = this.firstName.substring(0,1);
+    let patternOne = /[aeimquy]/gi;
+    let patternTwo = /[bfjnrvz]/gi;
+    let patternThree = /[cgkosw]/gi;
+    let patternFour = /[dhlptx]/gi;
+    let resultOne = theDecider.match(patternOne).toString();
+    let resultTwo = theDecider.match(patternOne).toString();
+    let resultThree = theDecider.match(patternOne).toString();
+    let resultFour = theDecider.match(patternOne).toString();
+    console.log(resultOne);
+    console.log(resultTwo);
+    console.log(resultThree);
+    console.log(resultFour);
+
+    switch (theDecider) {
+      case resultOne:
+        this.displayGod = 'red';
+        break;
+      case 'b': 
+        this.displayGod = 'orange';
+        break;
+      case 'c': 
+        this.displayGod = 'yellow';
+        break;
+      case 'd': 
+        this.displayGod = 'green';
+        break;
+      case 'e':
+        this.displayGod = 'blue';
+        break;
+      case 'f':
+        this.displayGod = 'indigo';
+        break;
+      case 'g': 
+        this.displayGod = 'violet';
+    } 
+    console.log(theDecider);
     console.log(this.firstName.substring(0,1))
-  }
-
-  stevieIsYourGod = () => {
-    let steve = document.createElement("img");
-    steve.src = "assets/stevieRayVaughn.png";
-    document.body.appendChild(steve);
-    let steveMsg = "You are Stevie Ray Vaughn... You are a GOD!";
-    let steveElement = document.createElement("h3");
-    steveElement.innerHTML = steveMsg;
-    document.body.appendChild(steveElement);
-  }
-
-  jimmyIsYourGod = () => {
-    let jimmy = document.createElement("img");
-    jimmy.src = "assets/jimmyPage.png";
-    document.body.appendChild(jimmy);
-    let jimmyMsg = "You are Jimmy Page... You are a GOD!";
-    let jimmyElement = document.createElement("h3");
-    jimmyElement.innerHTML = jimmyMsg;
-    document.body.appendChild(jimmyElement);
-  }
-
-  nancyIsYourGod = () => {
-    let nancy = document.createElement("img");
-    nancy.src = "assets/nancyWilson.png";
-    document.body.appendChild(nancy);
-    let nancyMsg = "You are Nancy Wilson... You are a GOD!";
-    let nancyElement = document.createElement("h3");
-    nancyElement.innerHTML = nancyMsg;
-    document.body.appendChild(nancyElement);
-  }
-
-  jimiIsYourGod = () => {
-    let jimi = document.createElement("img");
-    jimi.src = "assets/jimiHendrix.png";
-    document.body.appendChild(jimi);
-    let jimiMsg = "You are Jimi Hendrix... You are a GOD!";
-    let jimiElement = document.createElement("h3");
-    jimiElement.innerHTML = jimiMsg;
-    document.body.appendChild(jimiElement);
   }
 
   constructor() {}
