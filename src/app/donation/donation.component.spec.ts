b3f939d9a662d429db56407018e55165c5784c33
trogers9119 <tarogers1@madisoncollege.edu>
2020-10-15 19:33:00 -0500

    division functional...

--- src/app/donation/donation.component.spec.ts
+++ src/app/donation/donation.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TjsSubtractionComponent } from './tjs-subtraction.component';
+import { DonationComponent } from './donation.component';
 
-describe('TjsSubtractionComponent', () => {
-  let component: TjsSubtractionComponent;
-  let fixture: ComponentFixture<TjsSubtractionComponent>;
+describe('DonationComponent', () => {
+  let component: DonationComponent;
+  let fixture: ComponentFixture<DonationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TjsSubtractionComponent ]
+      declarations: [ DonationComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TjsSubtractionComponent);
+    fixture = TestBed.createComponent(DonationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
